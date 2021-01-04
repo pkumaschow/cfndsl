945509b2a0d036ad3fa40182822b524378c4e123
peterk <pkumashow@gmail.com>
2021-01-05 10:20:19 +1100

    Updating to 1.3.0

--- templates/test.rb
+++ templates/test.rb
@@ -12,29 +12,42 @@ CloudFormation {
     ImageId "ami-12345678"
   }
 
+  propertyVolumes = []
+  cdVolumes = {}
+  (cdVolumes['MountPoints'] ||= []) << {
+    ContainerPath: "/app/keys",
+    SourceVolume: "authorisation-keys",
+    Readonly: true
+  }
+  (cdVolumes['MountPoints'] ||= []) << {
+    ContainerPath: "/app/ssh-keys",
+    SourceVolume: "ssh-keys",
+    Readonly: true
+  }
+  propertyVolumes = [{
+    Name: "vol1",
+    Host: {
+      SourcePath: "/var/app/vol1"
+    }
+  }, {
+    Name: "vol2",
+    Host: {
+      SourcePath: "/var/app/vol2"
+    }
+  }]
+
+  propertyVolumes.push({
+    Name: "vol3",
+    Host: {
+      SourcePath: "/var/app/vol3"
+    }
+  })
+ 
   Resource(:myInstance2) do
     Type 'AWS::EC2::Instance'
     Property "ImageId", "ami-12345678"
     Property "Type", "t2.micro"
-    volumeProperties = [{
-      Name: "vol1",
-      Host: {
-        SourcePath: "/var/app/vol1"
-      }
-    }, {
-      Name: "vol2",
-      Host: {
-        SourcePath: "/var/app/vol2"
-      }
-    }]
-
-    volumeProperties.push({
-      Name: "vol3",
-      Host: {
-        SourcePath: "/var/app/vol3"
-      }
-    })
-    Property "Volumes", volumeProperties
+    Property "Volumes", propertyVolumes
   end
 
 }
