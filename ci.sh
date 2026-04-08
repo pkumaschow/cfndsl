be520e4e328bf707b10eb8b5eb337368d652edbe
pkumaschow <pkumaschow@gmail.com>
2026-04-08 21:30:25 +1000

    added podman argument to ci script

--- ci.sh
+++ ci.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+RUNTIME=docker
+if [ "${1}" = "podman" ]; then
+  RUNTIME=podman
+fi
+
 export CFNDSL_VERSION=1.7.3
 
 echo "Building cfndsl version $CFNDSL_VERSION"
 
-if [ -z ${CI}]; then
-  docker build --build-arg CFNDSL_VERSION=$CFNDSL_VERSION -t cfndsl:latest -t cfndsl:$CFNDSL_VERSION .
+if [ -z ${CI} ]; then
+  ${RUNTIME} build --build-arg CFNDSL_VERSION=$CFNDSL_VERSION -t cfndsl:latest -t cfndsl:$CFNDSL_VERSION .
 else
-  docker build --build-arg CFNDSL_VERSION=$CFNDSL_VERSION -t cfndsl:latest -t cfndsl:$CFNDSL_VERSION /godata/pipelines/docker-cfndsl/.
+  ${RUNTIME} build --build-arg CFNDSL_VERSION=$CFNDSL_VERSION -t cfndsl:latest -t cfndsl:$CFNDSL_VERSION /godata/pipelines/docker-cfndsl/.
 fi
